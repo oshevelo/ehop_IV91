6ded9e7cc6e4d40686a065d6d847a3ad9ebb2d4e
Alex Shevelo <alex.shevelo@gmail.com>
2022-04-21 19:45:40 +0300

    implement local setting stuff

--- ehop/ehop/settings.py
+++ ehop/ehop/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e()g$kb^2lq^hdr%u84u0a&(aqsz@id)9z)p%+rrrt82i=hr5l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,3 +121,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from .local_settings import *
