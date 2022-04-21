502935544c35932a1ae04be11204556640dad9fe
Alex Shevelo <alex.shevelo@gmail.com>
2022-04-21 20:24:50 +0300

    fix setting to use postgre

--- ehop/ehop/settings.py
+++ ehop/ehop/settings.py
@@ -72,9 +72,13 @@ WSGI_APPLICATION = 'ehop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': { 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': '', 
+        'USER': '', 
+        'PASSWORD': '', 
+        'HOST': 'localhost', 
+        'PORT': 5432, 
     }
 }
 
