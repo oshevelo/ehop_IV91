8b16a8150e78f5c582c1d1211542f210016170b6
Dazai <roma.domaratsky@gmail>
2022-04-22 21:08:00 +0300

    added userprofile model

--- ehop/apps/userprofiles/models.py
+++ ehop/apps/userprofiles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    GENDER_CHOICE = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Another', 'Another')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(blank=True, null=True)
+    email = models.EmailField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    gender = models.CharField(max_length=7, choices=GENDER_CHOICE, default='Another')
+
+    def __str__(self):
+        return f'{self.user} {self.first_name} {self.last_name} {self.gender} {self.email} {self.phone_number}'
+
